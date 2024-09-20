1553ce5303fd91bb566d5ff2f264c0033dda836e
pablobuenaposada <pablo.buenaposada@gmail.com>
2024-09-21 00:06:30 +0200

    add book model

--- src/library/models.py
+++ src/library/models.py
@@ -8,3 +8,15 @@ class Author(TimeStampedModel):
     def save(self, **kwargs):
         self.full_clean()
         super().save(**kwargs)
+
+
+class Book(TimeStampedModel):
+    title = models.CharField(max_length=200, null=False, blank=False, default=None)
+    author = models.ForeignKey(Author, related_name="books", on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["title", "author"], name="unique_book_author"
+            )
+        ]
--- src/tests/library/test_models.py
+++ src/tests/library/test_models.py
@@ -1,8 +1,9 @@
 import pytest
 from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from model_bakery import baker
 
-from library.models import Author
+from library.models import Author, Book
 
 
 @pytest.mark.django_db
@@ -20,5 +21,29 @@ class TestAuthor:
             "modified": author.modified,
             "name": author.name,
         }
-        for field in [field.name for field in Author._meta.get_fields()]:
+        for field in {field.name for field in Author._meta.get_fields()} - {"books"}:
             assert getattr(author, field) == expected[field]
+
+
+@pytest.mark.django_db
+class TestBook:
+    def test_mandatory_fields(self):
+        with pytest.raises(IntegrityError) as error:
+            Book.objects.create()
+
+        assert (
+            'null value in column "title" of relation "library_book" violates not-null constraint'
+            in str(error.value)
+        )
+
+    def test_valid(self):
+        book = baker.make(Book, title="foo", author=baker.make(Author))
+        expected = {
+            "id": book.id,
+            "created": book.created,
+            "modified": book.modified,
+            "title": book.title,
+            "author": book.author,
+        }
+        for field in {field.name for field in Book._meta.get_fields()}:
+            assert getattr(book, field) == expected[field]
